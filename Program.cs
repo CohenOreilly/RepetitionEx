c53cd67810ce9feec38d02f814f72bca435f10cf
CohenOreilly <80442810+CohenOreilly@users.noreply.github.com>
2021-03-25 16:18:25 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Repetiton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int num;
+            string input2;
+            int counter =0;
+
+
+
+            Console.WriteLine("how many repeats");
+            input2 = Console.ReadLine();
+
+            num = int.Parse(input2);
+            
+            
+
+            while ( counter < num ) {
+            Console.WriteLine("Name");
+            input = Console.ReadLine();
+            }
+
+            System.Console.WriteLine("goodbye");
+
+        }
+    }
+}
